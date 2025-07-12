4ecf02940fd450b08a081a682bdb7b3b5b95bfb5
Arthur <arthur.pradier81@gmail.com>
2025-07-12 20:52:49 +0200

    feature(Init): Initial commit
    Basic setup of the project: kind of functional CMakeLists, coarse project structure, ultra basic includes to be certain that everything works, gitignores. Ready to get to work now!

---
+++ Source/CicadaEngine/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Bellaedris on 12/07/2025.
+//
+
+#include <cstdio>
+
+int main()
+{
+    printf("Hello Cicada\n");
+
+    return 0;
+}
---
+++ Tests/helloTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Bellaedris on 12/07/2025.
+//
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
