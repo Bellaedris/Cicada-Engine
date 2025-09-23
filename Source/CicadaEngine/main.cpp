54894ab77701175feb362976d7bd95f52dcd68b2
Arthur <arthur.pradier81@gmail.com>
2025-09-23 22:33:25 +0200

    feature(Init): instance creation & validation layers
    title

--- Source/CicadaEngine/main.cpp
+++ Source/CicadaEngine/main.cpp
@@ -1,4 +1,5 @@
-import vulkan_hpp;
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_raii.hpp>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -6,9 +7,24 @@ import vulkan_hpp;
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+#include <algorithm>
+
 #pragma region Constants
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers
+        {
+            "VK_LAYER_KHRONOS_validation",
+        };
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
 #pragma endregion Constants
 
 class HelloTriangleApplication
@@ -25,8 +41,121 @@ public:
 private:
     #pragma region Members
     GLFWwindow* m_window;
+
+    vk::raii::Context m_context; // the context is automatically created by the raii constructor
+    vk::raii::Instance m_instance {nullptr};
+    vk::raii::DebugUtilsMessengerEXT debugMessenger {nullptr};
     #pragma endregion Members
 
+    #pragma region Methods
+    #pragma region Statics
+    static VKAPI_ATTR vk::Bool32 VKAPI_CALL DebugCallback(
+            vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+            vk::DebugUtilsMessageTypeFlagsEXT type,
+            const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData, void*)
+    {
+        std::cerr << "validation layer: type " << to_string(type) << " msg: " << pCallbackData->pMessage << std::endl;
+
+        return vk::False;
+    }
+    #pragma endregion Statics
+
+    void SetupDebugMessenger()
+    {
+        if(!enableValidationLayers)
+            return;
+
+        vk::DebugUtilsMessageSeverityFlagsEXT  severityFlags(vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eError
+        );
+
+        vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+            vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance
+        );
+
+        vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT
+        {
+            .messageSeverity = severityFlags,
+            .messageType = messageTypeFlags,
+            .pfnUserCallback = &DebugCallback
+            //.pUserData = could be used to pass a pointer to arbitrary data, for instance our renderer class, or something
+        };
+
+        debugMessenger = m_instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+    }
+
+    void CreateInstance()
+    {
+        constexpr vk::ApplicationInfo appInfo {
+            .pApplicationName = "Cicada Engine",
+            .applicationVersion = VK_MAKE_VERSION(0, 0, 1),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(0, 0, 1),
+            .apiVersion = vk::ApiVersion14
+        };
+
+        std::vector<const char*> requiredExtensions = GetRequiredExtensions();
+        uint32_t extensionsCount = requiredExtensions.size();
+
+        // retrieve the available extensions and ensure we have the ones required by glfw
+        std::vector<vk::ExtensionProperties> extensionProperties =  m_context.enumerateInstanceExtensionProperties();
+        for(uint32_t i = 0; i < extensionsCount; i++)
+        {
+            if(std::ranges::none_of(extensionProperties,
+                                    [glfwExtension = requiredExtensions[i]](auto const& extensionProperty)
+                                    { return strcmp(glfwExtension, extensionProperty.extensionName) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(requiredExtensions[i]));
+            }
+        }
+
+        // retrieve the available layers and ensure it includes the validation layers we need
+        std::vector<const char*> requiredLayers;
+        if(enableValidationLayers)
+        {
+            requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+        }
+
+        std::vector<vk::LayerProperties> layerProperties = m_context.enumerateInstanceLayerProperties();
+        for(const char* layer : requiredLayers)
+        {
+            if(std::ranges::none_of(layerProperties,
+                                    [requiredLayer = layer](auto const& layerProperty)
+                                    { return std::strcmp(requiredLayer, layerProperty.layerName) == 0; }))
+            {
+                throw std::runtime_error("Required layer " + std::string(layer) + "not supported");
+            }
+        }
+
+        vk::InstanceCreateInfo createInfo {
+            .pApplicationInfo = &appInfo,
+            .enabledLayerCount = static_cast<uint32_t>(requiredLayers.size()),
+            .ppEnabledLayerNames = requiredLayers.data(),
+            .enabledExtensionCount = extensionsCount,
+            .ppEnabledExtensionNames = requiredExtensions.data(),
+        };
+
+        m_instance = vk::raii::Instance(m_context, createInfo);
+    }
+
+    std::vector<const char*> GetRequiredExtensions()
+    {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        if(enableValidationLayers)
+        {
+            extensions.push_back(vk::EXTDebugUtilsExtensionName);
+        }
+
+        return extensions;
+    }
+    #pragma endregion Methods
+
+    #pragma region Lifecycle
     void InitWindow()
     {
         glfwInit();
@@ -35,12 +164,12 @@ private:
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         m_window = glfwCreateWindow(WIDTH, HEIGHT, "Cicada", nullptr, nullptr);
-
     }
 
     void InitVulkan()
     {
-
+        CreateInstance();
+        SetupDebugMessenger();
     }
 
     void MainLoop()
@@ -57,6 +186,7 @@ private:
 
         glfwTerminate();
     }
+    #pragma endregion Lifecycle
 };
 
 int main() {
