254714184349c2d5675e73cb597cf9b963388a80
bellaedris <bellpradier@proton.me>
2025-10-10 15:08:40 +0200

    feat(Swapchain): KHR surface + swapchain creation
    It's all in the title

--- Source/CicadaEngine/main.cpp
+++ Source/CicadaEngine/main.cpp
@@ -52,10 +52,16 @@ private:
 
     vk::raii::Context m_context; // the context is automatically created by the raii constructor
     vk::raii::Instance m_instance {nullptr};
-    vk::raii::DebugUtilsMessengerEXT debugMessenger {nullptr};
+    vk::raii::DebugUtilsMessengerEXT m_debugMessenger {nullptr};
+    vk::raii::SurfaceKHR m_surface {nullptr};
     vk::raii::PhysicalDevice m_physicalDevice {nullptr};
     vk::raii::Device m_device {nullptr};
     vk::raii::Queue m_graphicsQueue {nullptr};
+    vk::raii::Queue m_presentQueue {nullptr};
+    vk::raii::SwapchainKHR m_swapChain {nullptr};
+    std::vector<vk::Image> m_swapChainImages;
+    vk::Format m_swapChainImageFormat{vk::Format::eUndefined};
+    vk::Extent2D m_swapChainExtent;
     #pragma endregion Members
 
     #pragma region Methods
@@ -94,7 +100,7 @@ private:
             //.pUserData = could be used to pass a pointer to arbitrary data, for instance our renderer class, or something
         };
 
-        debugMessenger = m_instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+        m_debugMessenger = m_instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
     }
 
     void CreateInstance()
@@ -239,15 +245,23 @@ private:
     {
         std::vector<vk::QueueFamilyProperties> queueFamilyProperties = m_physicalDevice.getQueueFamilyProperties();
 
-        // get the first index into queueFamilyProperties which supports graphics
-        auto graphicsQueueFamilyProperty = std::ranges::find_if( queueFamilyProperties, []( auto const & qfp )
-                        { return (qfp.queueFlags & vk::QueueFlagBits::eGraphics) != static_cast<vk::QueueFlags>(0); } );
-        if (graphicsQueueFamilyProperty == queueFamilyProperties.end())
-        {
-            throw std::runtime_error("Couldn't find a graphics queue.");
-        }
+        // TODO eventually refactor this to allow for selection of a present-only queue if no queue supports both graphics/present
+        // overall try to separate the errors to pinpoint where the issue arose (graphics or present?)
 
-        auto graphicsIndex = static_cast<uint32_t>( std::distance( queueFamilyProperties.begin(), graphicsQueueFamilyProperty ) );
+        // get the first index into queueFamilyProperties which supports graphics
+        uint32_t graphicsIndex = -1;
+        for (int i = 0; i < queueFamilyProperties.size(); i++)
+        {
+            if ((queueFamilyProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) && m_physicalDevice.getSurfaceSupportKHR(i, m_surface))
+            {
+                graphicsIndex = i;
+                break;
+            }
+        }
+        if (graphicsIndex == -1)
+        {
+            throw std::runtime_error("Couldn't find a queue for graphics and present.");
+        }
 
         float queuePriority = 0.f;
         vk::DeviceQueueCreateInfo queueCreateInfo =
@@ -276,6 +290,95 @@ private:
         m_device = vk::raii::Device(m_physicalDevice, deviceCreateInfo);
 
         m_graphicsQueue = vk::raii::Queue(m_device, graphicsIndex, 0);
+        m_presentQueue = vk::raii::Queue(m_device, graphicsIndex, 0);
+    }
+
+    void CreateSurface()
+    {
+        VkSurfaceKHR surface;
+        if (glfwCreateWindowSurface(*m_instance, m_window, nullptr, &surface) != 0)
+        {
+            throw std::runtime_error("Couldn't create window surface.");
+        }
+        m_surface = vk::raii::SurfaceKHR(m_instance, surface);
+    }
+
+    vk::SurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<vk::SurfaceFormatKHR>& availableFormats)
+    {
+        for (const auto& availableFormat : availableFormats)
+        {
+            if (availableFormat.format == vk::Format::eB8G8R8A8Srgb && availableFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear)
+            {
+                return availableFormat;
+            }
+        }
+
+        return availableFormats[0];
+    }
+
+    /**
+     * Returns the most suitable present mode from a list of available ones in a surface attached to a device
+     * @param availablePresentModes a list of all present modes compatible with this device/surface combination
+     * @return Mailbox if found, fifo otherwise
+     */
+    vk::PresentModeKHR ChooseSwapPresentMode(const std::vector<vk::PresentModeKHR>& availablePresentModes)
+    {
+        for (const auto& availablePresentMode : availablePresentModes)
+        {
+            if (availablePresentMode == vk::PresentModeKHR::eMailbox)
+                return availablePresentMode;
+        }
+        return vk::PresentModeKHR::eFifo;
+    }
+
+    vk::Extent2D ChooseSwapExtent(const vk::SurfaceCapabilitiesKHR& capabilities)
+    {
+        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+            return capabilities.currentExtent;
+
+        int width, height;
+        glfwGetFramebufferSize(m_window, &width, &height);
+
+        return
+        {
+            std::clamp<uint32_t>(width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+            std::clamp<uint32_t>(height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height)
+        };
+    }
+
+    void CreateSwapChain()
+    {
+        vk::SurfaceCapabilitiesKHR surfaceCapabilities = m_physicalDevice.getSurfaceCapabilitiesKHR(m_surface);
+        vk::SurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(m_physicalDevice.getSurfaceFormatsKHR(m_surface));
+        vk::Extent2D extent = ChooseSwapExtent(surfaceCapabilities);
+        unsigned int swapChainImageCount = std::max(3u, surfaceCapabilities.minImageCount);
+        // maxImageCount == 0 means that there is no upper limit
+        swapChainImageCount = surfaceCapabilities.maxImageCount == 0 ? swapChainImageCount : std::min(swapChainImageCount, surfaceCapabilities.maxImageCount);
+
+        vk::SwapchainCreateInfoKHR swapchainCreateInfo
+        {
+            .flags = vk::SwapchainCreateFlagsKHR(),
+            .surface = m_surface,
+            .minImageCount = swapChainImageCount,
+            .imageFormat = surfaceFormat.format,
+            .imageColorSpace = surfaceFormat.colorSpace,
+            .imageExtent = extent,
+            .imageArrayLayers = 1, // always 1 unless steroscopic 3D
+            .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+            .imageSharingMode = vk::SharingMode::eExclusive, // change if graphics & present queues are different (to concurrent). Alternatively, keep exclusive but we will have to move ownerships
+            .queueFamilyIndexCount = 0, // change if graphcis & present are different (change to 2)
+            .pQueueFamilyIndices = nullptr, // change to an array of the queue indices if graphics & present are different
+            .preTransform = surfaceCapabilities.currentTransform,
+            .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+            .clipped = true,
+            .oldSwapchain = nullptr // pass the old swapchain in case of recreation, for instance when changing resolution
+        };
+
+        m_swapChainImageFormat = surfaceFormat.format;
+        m_swapChainExtent = extent;
+
+        m_swapChain = vk::raii::SwapchainKHR(m_device, swapchainCreateInfo);
+        m_swapChainImages = m_swapChain.getImages();
     }
     #pragma endregion Methods
 
@@ -294,8 +397,10 @@ private:
     {
         CreateInstance();
         SetupDebugMessenger();
+        CreateSurface();
         PickPhysicalDevice();
         CreateLogicalDevice();
+        CreateSwapChain();
     }
 
     void MainLoop()
